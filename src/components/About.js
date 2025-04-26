5f4f89288570d80f914b19cec5d334b1ec652479
Evian1k <emmanuelevian@gmail.com>
2025-04-26 10:31:06 +0300

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,14 @@
 import React from "react";
 
-function About({
-  image = "https://avatars.githubusercontent.com/u/197304308?s=400&u=8472ead44bf96d79639f5c00c082a4426cb69b27&v=4",
-  about,
-}) {
+function About({ image = "https://avatars.githubusercontent.com/u/197304308?s=400&u=8472ead44bf96d79639f5c00c082a4426cb69b27&v=4", about }) {
   return (
     <aside>
       <a
-        href="https://github.com/EmmanuelEvian" // Links to your GitHub profile
-        target="_blank" // Opens in a new tab
-        rel="noopener noreferrer" // Security best practice
+        href="https://github.com/EmmanuelEvian"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <img
-          src={image}
-          alt="Emmanuel Evian's GitHub profile"
-          style={{ borderRadius: "50%", width: "150px", height: "150px" }} // Optional: gives the image a rounded appearance
-        />
+        <img src={image} alt="blog logo" />
       </a>
       <p>{about}</p>
     </aside>
